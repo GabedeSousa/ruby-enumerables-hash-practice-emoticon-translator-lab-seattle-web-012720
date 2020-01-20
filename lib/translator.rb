47b87c6060720d74e8d31266a7a019b221373636
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-20 21:31:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,6 +24,7 @@ def get_japanese_emoticon(emoticon_file, emoticon)
 def get_english_meaning(file_path, emoticon)
   hash = load_library(file_path)
   hash['get_meaning'].each do |jap_emot,meaning|
-    return meaning if jap_emot =
+    return meaning if jap_emot == emoticon
+    
   
 end
