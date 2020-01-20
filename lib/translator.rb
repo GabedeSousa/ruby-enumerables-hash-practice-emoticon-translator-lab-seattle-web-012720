9862d50730da6f2bede46061951e2d12e410a86e
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-20 21:26:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,9 +18,8 @@ def get_japanese_emoticon(emoticon_file, emoticon)
   emoticons_lib = load_library(emoticon_file)
   engllish_meaning = emoticons_lib['get_meaning'][emoticon]
   engllish_meaning ? engllish_meaning : 'Sorry, that emoticon was not found'
+  end
   
-  
-end
 
 def get_english_meaning
   # code goes here
