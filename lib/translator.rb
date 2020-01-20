d175c61f97fc1047bb25d6dedcced2353dbce57d
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-20 21:30:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,6 +23,6 @@ def get_japanese_emoticon(emoticon_file, emoticon)
 
 def get_english_meaning(file_path, emoticon)
   hash = load_library(file_path)
-  hash['get_meaning']
+  hash['get_meaning'].each do 
   
 end
